55d2f1f69e09e7e6ecde12fe0f6cdef69c5c78cd
Abdulshakour_Aref <87705608+Shiko3030@users.noreply.github.com>
2024-09-16 14:57:54 +0300

    Update urls.py

--- room/chapter/urls.py
+++ room/chapter/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     
-path('',views.home,name=''),
+path('',views.index,name=''),
 path('add_note/',views.add_note,name='add_note'),
 path('edit/<str:pk>/',views.edit,name='edit'),
 path('delete/<str:pk>/',views.delete,name='delete'),
